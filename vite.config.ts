9932acc0e56b7928bc1a7548905c080adad6eaff
crow <petrgrigorec32@gmail.com>
2025-02-24 01:06:46 +0300

    add docker config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-
+// import TsconfigPathsPlugin from 'vite-plugin-tsconfig-paths';
+// import javascriptObfuscator from 'vite-plugin-javascript-obfuscator';
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // TsconfigPathsPlugin(),
+    // javascriptObfuscator({
+    //   options: {
+    //     compact: false,
+    //     controlFlowFlattening: true,
+    //     controlFlowFlatteningThreshold: 1,
+    //     numbersToExpressions: true,
+    //     simplify: true,
+    //     stringArrayShuffle: true,
+    //     splitStrings: true,
+    //     stringArrayThreshold: 1,
+    //     selfDefending: true,
+    //     stringArray: true,
+    //     stringArrayEncoding: ['base64'],
+    //     unicodeEscapeSequence: true,
+    //   },
+    // }),
+  ],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    rollupOptions: {
+      plugins: [],
+    },
+  },
   css: {
     postcss: './postcss.config.js',
   },
@@ -12,4 +39,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    port: 8080,
+    watch: {
+      usePolling: true
+    }
+  }
 })
